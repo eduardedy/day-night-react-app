b7727f68445a1fbab596e616d06f62a20dfcb30d
eduardedy <33651687+eduardedy@users.noreply.github.com>
2019-08-25 21:07:11 +0300

    day-night-react-app commit

--- src/components/MapContainer .js
+++ src/components/MapContainer .js
@@ -1,8 +1,5 @@
 import React from "react";
-import { GoogleApiWrapper, Map , InfoWindow, Marker } from "google-maps-react"; 
- 
-
-
+import { GoogleApiWrapper, Map, InfoWindow, Marker } from "google-maps-react";
 class MapContainer extends React.Component {
   constructor(props) {
     super(props);
@@ -14,56 +11,66 @@ class MapContainer extends React.Component {
         {
           title: "The marker`s title will appear as a tooltip.",
           name: "SOMA",
-          position: { lat: 44.913539334255155, lng: 26.024701314458866 }
+          position: { lat: 40.854885, lng: -88.081807 }
         }
-      ], 
-      sunrise:'',
-      sunset: '',
-      timeAbroad: '',
-      dayNight: ''
+      ],
+      sunrise: "",
+      sunset: "",
+      timeAbroad: "",
+      dayNight: "",
+      center: { lat: 40.854885, lng: -88.081807 },
+      check: 1
     };
     this.onClick = this.onClick.bind(this);
   }
 
   onClick(t, map, coord) {
-    const { latLng } = coord;
-    const lat =  latLng.lat();
-    const lng =   latLng.lng();
+    if (this.state.check == 1) {
+      this.getNewLocation();
+    } else {
+      const { latLng } = coord;
+      const lat = latLng.lat();
+      const lng = latLng.lng();
 
-    // get new location time  
-    var apiKey2 = 'SC4L9GNRWSOC'
-    var url2 = `http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey2}&format=json&by=position&lat=${lat}&lng=${lng}`
-     fetch(url2)  
-    .then(res => res.json())        
-    .then(timezone => { 
-      let locSelectedTime = new Date(timezone.formatted).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
-      this.setState({timeAbroad: locSelectedTime}) 
-    }) 
- 
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      })
-    }
-  
-    // get new location certain data
-    const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`
-    fetch(url)  
-        .then(res => res.json())        
-        .then(text => {   
+      // get new location time
+      var apiKey2 = "SC4L9GNRWSOC";
+      var url2 = `http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey2}&format=json&by=position&lat=${lat}&lng=${lng}`;
+      fetch(url2)
+        .then(res => res.json())
+        .then(timezone => {
+          let locSelectedTime = new Date(timezone.formatted)
+            .toLocaleTimeString()
+            .replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+          this.setState({ timeAbroad: locSelectedTime });
+        });
+
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        });
+      }
+
+      // get new location certain data
+      const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`;
+      fetch(url)
+        .then(res => res.json())
+        .then(text => {
           let dayNight;
-
-          if(this.state.timeAbroad >= text.results.sunrise   &&  this.state.timeAbroad <  text.results.sunset){
-            dayNight = 'Day'
-          }else{
-            dayNight = 'Night'
+          if (
+            this.state.timeAbroad >= text.results.sunrise &&
+            this.state.timeAbroad < text.results.sunset
+          ) {
+            dayNight = "Day";
+          } else {
+            dayNight = "Night";
           }
-    
+
           this.setState(previousState => {
-            return { 
-              markers: [ 
-                {  
+            return {
+              center: { lat, lng },
+              markers: [
+                {
                   name: dayNight,
                   position: { lat, lng }
                 }
@@ -72,52 +79,60 @@ class MapContainer extends React.Component {
               sunset: text.results.sunset,
               dayNight: dayNight
             };
-          });  
-        })  
-     
+          });
+        });
+    }
   }
- 
 
-  test = () => { 
-    console.log(this.state) 
-  }
- 
+  getNewLocation = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+
+      this.setState({
+        check: 2,
+        center: { lat: latitude, lng: longitude },
+        markers: [
+          {
+            position: { lat: latitude, lng: longitude }
+          }
+        ]
+      });
+    });
+  };
 
   onMarkerClick = (props, marker, e) => {
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
       showingInfoWindow: true
-    }); 
-  }
- 
-  onMapClicked = (props) => {
+    });
+  };
+
+  onMapClicked = props => {
     if (this.state.showingInfoWindow) {
       this.setState({
         showingInfoWindow: false,
         activeMarker: null
-      })
+      });
     }
-
-
-   
   };
 
   render() {
-
-    console.log(this.state)
-
-   // const {userLocation} = this.state
+    console.log(this.state);
     return (
-      <div> 
-        <h1 className="text-center" onClick={this.test}>My Maps</h1>
+      <div>
+        <h1 className="text-center">My Test Map</h1>
         <Map
           google={this.props.google}
           style={{ width: "80%", margin: "auto" }}
           className={"map"}
           zoom={14}
-      initialCenter={this.state.markers[0].position}
-          onClick={  this.onClick   }
+          onClick={this.onClick}
+          initialCenter={{
+            lat: 40.854885,
+            lng: -88.081807
+          }}
+          center={this.state.center}
         >
           {this.state.markers.map((marker, index) => (
             <Marker
@@ -129,22 +144,19 @@ class MapContainer extends React.Component {
             />
           ))}
 
-<InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+          >
             <div>
               <h1>{this.state.selectedPlace.name}</h1>
             </div>
-        </InfoWindow>
-
-
+          </InfoWindow>
         </Map>
       </div>
     );
   }
 }
-
-
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyChYfdaurKnRjh0S4FjeKpaOIBk6sP-egs"
